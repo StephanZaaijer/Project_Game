c601197a02cc6eb93edb4175252a08f00d047da5
StephanZaaijer <stephan.zaaijer@gmail.com>
2022-01-25 10:46:12 +0100

    Update Game.cpp

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -15,33 +15,26 @@ Game::Game(const int &screen_width, const int &screen_height, const std::string 
 }
 
 void Game::start() {
-    try {
-        float new_time, frameTime;
-
-        float current_time = clock.getElapsedTime().asSeconds();
-        float accumulator = 0.0;
-
-        while (game_data->window.isOpen()) {
-            game_data->machine.ProcessGameStateChanges();
-            new_time = clock.getElapsedTime().asSeconds();
-            frameTime = new_time - current_time;
-            if (frameTime > 0.25) {
-                frameTime = 0.25;
-            }
-            current_time = new_time;
-            accumulator += frameTime;
-
-            while (accumulator >= delta) {
-                game_data->machine.GetActiveGameState()->HandleInput();
-                game_data->machine.GetActiveGameState()->Update(delta);
-                accumulator -= delta;
-            }
-            game_data->machine.GetActiveGameState()->Draw(accumulator / delta);
-            sf::sleep(sf::milliseconds(20));
+    float new_time, frameTime;
+    float current_time = clock.getElapsedTime().asSeconds();
+    float accumulator = 0.0;
+    while (game_data->window.isOpen()) {
+        game_data->machine.ProcessGameStateChanges();
+        new_time = clock.getElapsedTime().asSeconds();
+        frameTime = new_time - current_time;
+        if (frameTime > 0.25) {
+            frameTime = 0.25;
         }
-    }
-    catch (std::exception e) {
-        std::cout << e.what() << '\n';
+        current_time = new_time;
+        accumulator += frameTime;
+
+        while (accumulator >= delta) {
+            game_data->machine.GetActiveGameState()->HandleInput();
+            game_data->machine.GetActiveGameState()->Update(delta);
+        accumulator -= delta;
+        }
+        game_data->machine.GetActiveGameState()->Draw(accumulator / delta);
+        sf::sleep(sf::milliseconds(20));
     }
 }
 

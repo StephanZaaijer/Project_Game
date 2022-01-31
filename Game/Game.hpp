15728cddfbd867bb127e21f385e95e8883be80e4
StephanZaaijer <stephan.zaaijer@gmail.com>
2022-01-31 09:22:36 +0100

    added Doxygen to Game.hpp

--- Game/Game.hpp
+++ Game/Game.hpp
@@ -8,6 +8,13 @@
 #include "Definitions.hpp"
 #include "JsonManager.hpp"
 
+/// @file Game.hpp
+/// @brief
+/// Project_Game: Game class that constructs all the machines, the window and contains the gameloop
+
+
+/// @brief
+/// struct that contains all the Machines, the window and the score.
 struct GameData {
     StateMachine machine;
     sf::RenderWindow window;
@@ -17,16 +24,21 @@ struct GameData {
     int score = 0;
 };
 
+/// Typedefinition to define std::shared_prt<GameData> as GameDataReference
 typedef std::shared_ptr<GameData> GameDataReference;
 
 class Game {
 private:
     const float delta = 1.0/ FRAMERATE;
     sf::Clock clock;
-
     GameDataReference game_data = std::make_shared<GameData>();
     void start();
 public:
+    /// @brief
+    /// constructor that creates the window, and loads the StartUpstate into the statemachine.
+    /// \param screen_width The width of the renderwindow
+    /// \param screen_height The height of the renderwindow
+    /// \param game_title The title of the game shown in the menubar of the window
     Game(const int &screen_width, const int &screen_height, const std::string &game_title);
 };
 

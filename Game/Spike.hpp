36d823d318360ef909153d275481a2cb422e35f2
Jasper <jaspervdpost@gmail.com>
2022-02-01 16:50:09 +0100

    code cleanup, crashes while playing, need fix

--- Game/Spike.hpp
+++ Game/Spike.hpp
@@ -11,16 +11,13 @@
 class Spike : public Obstacle {
 private:
     GameDataReference game_data;
-//    sf::CircleShape Triangle = sf::CircleShape(SPIKE_SIZE, 3);
     sf::ConvexShape Triangle;
-
     Spike_facing facing;
-    sf::Vector2f position;
 
 public:
     Spike(GameDataReference game_data, Spike_facing facing, sf::Vector2f position);
     sf::Vector2f getPosition() override;
-    void spawn() override;
+//    void spawn() override;
     void move(sf::Vector2f move_by) override;
     void draw() const override;
     sf::FloatRect getBounds() override;

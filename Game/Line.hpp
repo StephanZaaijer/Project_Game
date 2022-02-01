edf1bb34a3f99b2e84785224ba44b7ccbdbb2529
Jasper <jaspervdpost@gmail.com>
2022-02-01 16:18:20 +0100

    IT WORKS, colision is complete

--- Game/Line.hpp
+++ Game/Line.hpp
@@ -15,17 +15,13 @@ struct formula{
     float rc;
     float offset;
 
-    float get_y_intersection(const sf::Vector2f & reference_point) {
-        float y = (rc * reference_point.x) + offset;
-
+    float get_y_intersection(const float & x){
+        float y = (rc * x) + offset;
         return y;
     }
 
-    float get_x_intersection(const sf::Vector2f & reference_point) {
-//        rc * x + offset = reference_point.y;
-//        rc * x = reference_point.y + (offset * -1);
-        float x = (reference_point.y + (offset * -1)) / rc;
-
+    float get_x_intersection(const float & y) {
+        float x = (y + (offset * -1)) / rc;
         return x;
     }
 };
@@ -33,20 +29,17 @@ struct formula{
 
 class Line {
 private:
+    std::vector<sf::Vector2f> points_array = {};
+
     sf::Vector2f A;
     sf::Vector2f B;
 
 public:
     Line(const sf::Vector2f & pointA = sf::Vector2f {0.0, 0.0}, const sf::Vector2f & pointB = sf::Vector2f {0.0, 0.0});
 
-    void setPointA(const sf::Vector2f & pointA);
-    void setPointA(const float & x, const float & y);
-
-    void setPointB(const sf::Vector2f & pointB);
-    void setPointB(const float & x, const float & y);
 
     formula calculateFormula();
-
+    std::vector<sf::Vector2f> & get_points();
     axisIntersection intersects(const sf::Vector2f & reference_point);
 
 

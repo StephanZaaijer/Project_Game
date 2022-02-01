01dc01e11b1461b6e5818098805245d6b25f9552
Jasper <jaspervdpost@gmail.com>
2022-02-01 16:55:16 +0100

    stable version, able to merge with dev

--- Game/Spike.hpp
+++ Game/Spike.hpp
@@ -12,12 +12,14 @@ class Spike : public Obstacle {
 private:
     GameDataReference game_data;
     sf::ConvexShape Triangle;
+
     Spike_facing facing;
+    sf::Vector2f position;
 
 public:
     Spike(GameDataReference game_data, Spike_facing facing, sf::Vector2f position);
     sf::Vector2f getPosition() override;
-//    void spawn() override;
+    void spawn() override;
     void move(sf::Vector2f move_by) override;
     void draw() const override;
     sf::FloatRect getBounds() override;

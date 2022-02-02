9a777bcf2983a3cd963e2b0f72b6a79b6aee2a08
Kalong98 <68122498+Kalong98@users.noreply.github.com>
2022-02-02 10:29:10 +0100

    Merge branch 'change_variables_names' of D:\HU\TI_Software_2\Project_Game with conflicts.

--- Game/Coin.hpp
+++ Game/Coin.hpp
@@ -6,16 +6,26 @@
 #include "Game.hpp"
 #include "AssetManager.hpp"
 
+/// @file Coin.hpp
+/// @brief
+/// Project_Game: Defines a coin object
+
 class Coin{
 private:
-    GameDataReference gameData;
-    sf::Sprite coin;
+    GameDataReference game_data;
+    sf::Sprite _coin;
 
 public:
-    Coin(GameDataReference gameData);
-    void move(sf::Vector2f moveBy);
-    void setPosition(sf::Vector2f position);
-    sf::Sprite getCoin();
+    /// @brief
+    /// Used to construct coin object.
+    /// @details
+    /// Used to construct coin object and initializes the object with a texture and scale
+    /// \param game_data .
+    /// \return The sf::Font that`s stored with that particular key.
+    Coin(GameDataReference game_data);
+    void move(sf::Vector2f move_by);
+    void set_position(sf::Vector2f position);
+    sf::Sprite get_coin();
     sf::FloatRect getBounds();
     sf::Vector2f getPosition();
     void draw();

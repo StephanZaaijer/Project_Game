c8a04225c37b7d9efcf492f9e6d3bdebb77a4767
Kalong98 <68122498+Kalong98@users.noreply.github.com>
2022-02-02 11:43:37 +0100

    Update Coin.hpp

--- Game/Coin.hpp
+++ Game/Coin.hpp
@@ -12,21 +12,43 @@
 
 class Coin{
 private:
-    GameDataReference game_data;
-    sf::Sprite _coin;
-
+    GameDataReference gameData;
+    sf::Sprite coin;
 public:
     /// @brief
     /// Used to construct coin object.
     /// @details
     /// Used to construct coin object and initializes the object with a texture and scale
-    /// \param game_data .
-    /// \return The sf::Font that`s stored with that particular key.
-    Coin(GameDataReference game_data);
-    void move(sf::Vector2f move_by);
-    void set_position(sf::Vector2f position);
-    sf::Sprite get_coin();
+    /// \param game_data is a shared pointer to the GameDataReference where information of the game is stored
+    Coin(GameDataReference gameData);
+
+    /// @brief
+    /// Used to move coin object.
+    /// @details
+    /// Used to move coin object by an offset given in the parameter.
+    /// \param moveBy is a sf vector which contains a X and Y coordinate
+    void move(sf::Vector2f moveBy);
+
+    /// @brief
+    /// Used to position the coin object.
+    /// @details
+    /// Used to overwrite the position of the coin with the given the parameter.
+    /// \param position is a sf vector which contains a X and Y coordinate
+    void setPosition(sf::Vector2f position);
+
+    /// @brief
+    /// Returns coin sprite.
+    /// \return Returns the sf::sprite of coin.
+    sf::Sprite getCoin();
+
+    /// @brief
+    /// Returns the global bounds of coin.
+    /// \return Returns a sf::Floatrect of the sprite coin.
     sf::FloatRect getBounds();
+
+    /// @brief
+    /// Returns the position of the coin sprite.
+    /// \return Returns a sf::Vector2f with the coordinates of the sprite coin.
     sf::Vector2f getPosition();
     void draw();
 };

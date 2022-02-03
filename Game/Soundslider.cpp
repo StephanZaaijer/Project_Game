b213d4b1be6fbe8078164f1b26b2fae4be85ecbd
StephanZaaijer <stephan.zaaijer@gmail.com>
2022-02-03 11:36:43 +0100

    Changed working of the Sliderclasses and added doxygen

--- Game/Soundslider.cpp
+++ Game/Soundslider.cpp
@@ -2,68 +2,37 @@
 #include "Exceptions.hpp"
 #include <iostream>
 
-SoundSlider::SoundSlider(GameDataReference gameData, sf::Vector2f sliderLocation, int length, bool horizontal, sf::Color sliderColorOn, sf::Color sliderColorOff, sf::Vector2f textLocation, int fontSize) :
-	Slider(gameData, sliderLocation, length, horizontal, sliderColorOn),
-	sliderColorOn(sliderColorOn),
-	sliderColorOff(sliderColorOff)
-{
-	prevState = gameData->json.getSoundState();
-	if (!prevState) {
-		sliderBlock.setFillColor(sliderColorOff);
-	}
-	if (horizontal) {
-		sliderMin = sliderLocation.x;
-		sliderMax = sliderLocation.x + length;
-		ratio = ((sliderMax - sliderMin) / 100);
-		prevSliderblockLocation = { sliderLocation.x + length - (gameData->json.getSoundVolume()) * ratio, sliderLocation.y };
-	}
-	else {
-		sliderMin = sliderLocation.y;
-		sliderMax = sliderLocation.y + length;
-		ratio = ((sliderMax - sliderMin) / 100);
-		prevSliderblockLocation = { sliderLocation.x, sliderLocation.y + length - (gameData->json.getSoundVolume()) *  ratio};
-
-	}
-	sliderBlock.setPosition(prevSliderblockLocation);
-	level.setFont(gameData->assets.getFont("Bauhaus"));
-	level.setPosition(textLocation);
-	level.setCharacterSize(fontSize);
-	level.setString(std::to_string(gameData->json.getSoundVolume()));
-	level.setOrigin({ level.getGlobalBounds().width/2, level.getGlobalBounds().height/2});
+SoundSlider::SoundSlider(GameDataReference gameData, sf::Vector2f sliderLocation, int length, bool horizontal, sf::Color sliderBlockColorOn, sf::Color sliderBlockColorOff, sf::Vector2f textLocation, int fontSize) :
+	Slider(gameData, sliderLocation, length, horizontal, sliderBlockColorOn, gameData->json.getSoundVolume()),
+    sliderBlockColorOn(sliderBlockColorOn),
+    sliderBlockColorOff(sliderBlockColorOff)
+    {
+        if (gameData->json.getSoundState()) {
+            setSliderBlockColor(sliderBlockColorOn);
+        } else {
+            setSliderBlockColor(sliderBlockColorOff);
+        }
+        level.setFont(gameData->assets.getFont("Bauhaus"));
+        level.setPosition(textLocation);
+        level.setCharacterSize(fontSize);
+        level.setString(std::to_string(gameData->json.getSoundVolume()));
+        level.setOrigin({level.getGlobalBounds().width / 2, level.getGlobalBounds().height / 2});
 }
 
 void SoundSlider::update() {
-	if (gameData->json.getSoundState() and !prevState) {
-		sliderBlock.setFillColor(sliderColorOn);
-		prevState = true;
-	}
-	else if (!gameData->json.getSoundState() and prevState) {
-		sliderBlock.setFillColor(sliderColorOff);
-		prevState = false;
-	}
-	if (prevSliderblockLocation != sliderBlock.getPosition()) {
-		if (horizontal) {
-			gameData->json.setSoundState(true);
-			int temp = sliderBlock.getPosition().x;
-			int currentLevel = 100 - (temp - sliderPoint.x) / ratio;
-			level.setString(std::to_string(currentLevel));
-			level.setOrigin({ level.getGlobalBounds().width / 2, level.getGlobalBounds().height / 2 });
-			gameData->json.setSoundVolume(currentLevel);
-		}
-		else {
-			gameData->json.setSoundState(true);
-			int temp = sliderBlock.getPosition().y;
-			int currentLevel = 100 - (temp - sliderPoint.y) / ratio;
-			level.setString(std::to_string(currentLevel));
-			level.setOrigin({ level.getGlobalBounds().width / 2, level.getGlobalBounds().height / 2 });
-			gameData->json.setSoundVolume(currentLevel);
-		}
-		prevSliderblockLocation = sliderBlock.getPosition();
-	}
+    if (gameData->json.getSoundState()) {
+        setSliderBlockColor(sliderBlockColorOn);
+    } else {
+        setSliderBlockColor(sliderBlockColorOff);
+    }
+}
+void SoundSlider::updateValue(int value) {
+    gameData->json.setSoundVolume(value);
+    level.setString(std::to_string(value));
+    level.setOrigin({ level.getGlobalBounds().width / 2, level.getGlobalBounds().height / 2 });
 }
 
 void SoundSlider::draw() {
-	gameData->window.draw(slider);
-	gameData->window.draw(sliderBlock);
+	Slider::draw();
 	gameData->window.draw(level);
 }
--- Game/Soundslider.hpp
+++ Game/Soundslider.hpp
@@ -3,22 +3,43 @@
 
 #include "Slider.hpp"
 
+///@file
+///@brief
+/// Project_Game: This is the SoundSlider which is used to change the soundvolume
+
+
+///@brief
+///SoundSliderclass that is implemented from Slider and is used to change the soundvolume in the SoundSettingsState
 class SoundSlider : public Slider
 {
 public:
-	SoundSlider(GameDataReference gameData, sf::Vector2f sliderLocation, int length, bool horizontal, sf::Color sliderColorOn, sf::Color sliderColorOff, sf::Vector2f textLocation, int fontSize);
+    /// @brief SoundSlider co
+    ///constructor that creates it`s superclass and set all the settings for the text and the color of the sliderBlock
+    /// \param gameData GameDataReference used to get the mouselocation and draw to the screen
+    /// \param sliderLocation sf::Vector2f which indicates where the slider is placed
+    /// \param length integer length of the slider in pixels
+    /// \param horizontal boolean that indicates if the slider is horizontal or vertical
+    /// \param sliderBlockColorOn the color that the sliderblock is when the sound is active
+    /// \param sliderBlockColorOff the color that the sliderblock is when the sound is inactive
+    /// \param textLocation the location of the soundvolume text
+    /// \param fontSize the fontsize of the soundvolume text
+	SoundSlider(GameDataReference gameData, sf::Vector2f sliderLocation, int length, bool horizontal, sf::Color sliderBlockColorOn, sf::Color sliderBlockColorOff, sf::Vector2f textLocation, int fontSize);
+    ///@brief
+    /// update function called by the state to update the color of the sliderBlock
     void update() override;
+    /// @brief
+    /// draw functions that draw`s the soundvolume text and calls the draw function of it`s superclass to draw the slider and the sliderblock
 	void draw() override;
 
 private:
-	sf::Color sliderColorOn;
-	sf::Color sliderColorOff;
-	int sliderMin;
-	int sliderMax;
-	bool prevState;
-	int ratio;
+	sf::Color sliderBlockColorOn;
+	sf::Color sliderBlockColorOff;
 	sf::Text level;
-	sf::Vector2f prevSliderblockLocation;
+    /// @brief
+    /// implemented virtual function of Slider called by the handleInputfunction of Slider to update the soundvolume value
+    /// \param value the new soundvolume value
+    void updateValue(int value) override;
+
 };
 
 #endif // PROJECT_GAME_SOUNDSLIDER
